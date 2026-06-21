5a56e12086e6a23e6b7c3c03d7659d318bbedb2d
Devpratap Singh <devprataprathore346@gmail.com>
2026-06-21 13:36:49 +0530

    Add Again Twenty Five! [A] from Codeforces

---
+++ 630/A - Again Twenty Five!/solution.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+ 
+signed main(){
+    int n;
+    cin >> n;
+ 
+    if(n>1){
+        cout << 25;
+    }else{
+        cout << 05;
+    }
+ 
+    return 0;
+}
